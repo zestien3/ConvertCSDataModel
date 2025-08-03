6af46eac39e36db3eebf9a455e9c77da975908bd
Peter Laudy <github@zestien3.nl>
2025-08-03 17:52:52 +0200

    Added "demo" command line verb.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using CommandLine;
 
 namespace Z3
@@ -13,7 +14,42 @@ namespace Z3
             TypeScript
         }
 
-        public class Options
+        internal interface Options
+        {
+            public bool AutoFind { get; set; }
+            public string? AssemblyName { get; set; }
+            public IEnumerable<Language>? Languages { get; set; }
+            public IEnumerable<string>? ClassNames { get; set; }
+            public string? OutputFolder { get; set; }
+        }
+
+        [Verb("demo", false, HelpText="Converts some included test classes as a demonstration")]
+        internal class DemoOptions : Options
+        {
+            [Option('a', "auto", Required = false, HelpText = "If defined, converts all classes with a UseInFrontend attribute")]
+            public bool AutoFind { get; set; }
+
+            [Option('f', "file", Required = false, HelpText = "The path to the Assembly which contains the class(es) to convert")]
+            public string? AssemblyName { get; set; }
+
+            [Option('l', "language", Required = false, HelpText = "The programming language(s) to convert to")]
+            public IEnumerable<Language>? Languages { get; set; }
+
+            [Option('c', "classes", Required = false, HelpText = "The name of the class(es) to convert (including the complete namespace)")]
+            public IEnumerable<string>? ClassNames { get; set; }
+
+            [Option('o', "out", Required = false, HelpText = "The folder to which the output files are written. If omitted, Console.Out is used")]
+            public string? OutputFolder { get; set; }
+
+            public DemoOptions()
+            {
+                AutoFind = true;
+                AssemblyName = Assembly.GetExecutingAssembly()!.Location;
+            }
+        }
+
+        [Verb("default", true)]
+        internal class DefaultOptions : Options
         {
             [Option('a', "auto", Required = false, HelpText = "If defined, converts all classes with a UseInFrontend attribute")]
             public bool AutoFind { get; set; }
@@ -38,48 +74,67 @@ namespace Z3
         [STAThread]
         public static void Main(string[] args)
         {
-            var parsed = Parser.Default.ParseArguments<Options>(args);
+            var parsed = Parser.Default.ParseArguments<DemoOptions, DefaultOptions>(args);
 
             if (parsed.Errors.Count() > 0)
             {
+                var assemblyName = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly()!.ManifestModule.Name);
+                Console.Error.WriteLine($"{assemblyName} converts classes found in the given .NET assembly to different programming languages.");
+                Console.Error.WriteLine($"It uses reflection to get the class information from the assembly.");
+                Console.Error.WriteLine($"{assemblyName} It uses reflection to get the class information from the assembly.");
                 Environment.Exit(1);
             }
 
-            cmdLine = parsed.Value;
-
-            List<string> classNames = new(cmdLine.ClassNames!);
-
-            assemblyInfo = MetadataAssemblyInfo.Factory(cmdLine.AssemblyName!);
-
-            if (cmdLine.AutoFind)
-            {
-                foreach (var classInfo in assemblyInfo.ClassesByName.Values)
+            cmdLine = parsed.MapResult(
+                (DemoOptions options) =>
                 {
-                    if (classInfo.Attributes.Contains("UseInFrontendAttribute"))
+                    using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ConvertCSDataModel.Demo.cs")!;
+                    var reader = new StreamReader(stream);
+                    Console.Out.WriteLine(reader.ReadToEnd());
+
+                    options.Languages = [Language.TypeScript];
+                    return (Options?)options;
+                },
+                (DefaultOptions options) => { return (Options?)options; },
+                _ => { return null; }
+            );
+
+            if (null != cmdLine)
+            {
+                List<string> classNames = new(cmdLine.ClassNames!);
+
+                assemblyInfo = MetadataAssemblyInfo.Factory(cmdLine.AssemblyName!);
+
+                if (cmdLine.AutoFind)
+                {
+                    foreach (var classInfo in assemblyInfo.ClassesByName.Values)
                     {
-                        if (!classNames.Contains(classInfo.FullName))
+                        if (classInfo.Attributes.Contains("UseInFrontendAttribute"))
                         {
-                            classNames.Add(classInfo.FullName);
+                            if (!classNames.Contains(classInfo.FullName))
+                            {
+                                classNames.Add(classInfo.FullName);
+                            }
                         }
                     }
                 }
-            }
 
-            foreach (var className in classNames)
-            {
-                var classInfo = assemblyInfo!.ClassesByName[className];
-
-                // For this class we do load more information.
-                classInfo.AllClassesLoaded(assemblyInfo, 2);
-                foreach (var language in cmdLine.Languages!)
+                foreach (var className in classNames)
                 {
-                    switch (language)
+                    var classInfo = assemblyInfo!.ClassesByName[className];
+
+                    // For this class we do load more information.
+                    classInfo.AllClassesLoaded(assemblyInfo, 2);
+                    foreach (var language in cmdLine.Languages!)
                     {
-                        case Language.TypeScript:
+                        switch (language)
                         {
-                            using var writer = GetOutput(TypeScriptFormatter.GetFileNameFromClass(classInfo));
-                            (new TypeScriptFormatter(writer)).FormatClass(classInfo);
-                            break;
+                            case Language.TypeScript:
+                                {
+                                    using var writer = GetOutput(TypeScriptFormatter.GetFileNameFromClass(classInfo));
+                                    (new TypeScriptFormatter(writer)).FormatClass(classInfo);
+                                    break;
+                                }
                         }
                     }
                 }
@@ -90,6 +145,7 @@ namespace Z3
         {
             if (string.IsNullOrEmpty(cmdLine!.OutputFolder))
             {
+                Console.Out.WriteLine();
                 return Console.Out;
             }
 
