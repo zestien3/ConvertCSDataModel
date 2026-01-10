adc7f5564b5283acbe721867f44b774fc0929dcd
Peter Laudy <github@zestien3.nl>
2026-01-10 11:20:20 +0000

    Starting to add a compact HTML dialog generator.

---
+++ src/Languages/HTML/HTMLCompactFormatter.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+
+using Zestien3;
+
+namespace Z3
+{
+    internal class HTMLCompactFormatter : HTMLFormatter
+    {
+        /// <summary>
+        /// Create an instance of the <see cref="HTMLFormatter"/> class.
+        /// </summary>
+        /// <param name="assemblyInfo">The assembly for which we create the output.</param>
+        /// <param name="output">The output to which the type script code must be written.</param>
+        public HTMLCompactFormatter(MetadataAssemblyInfo assemblyInfo, TextWriter output) : base(assemblyInfo, output)
+        {
+            buttonSectionOffset = 3;
+            buttonSectionWidth = 6;
+        }
+
+        protected override void WriteMemberInfo(MetadataMemberInfo info)
+        {
+            var indent = 3;
+
+            var addHideOptionForMember = ClassInfo!.Any(c =>
+                {
+                    if (c.UseInFrontend.ContainsKey(Language.HTML))
+                    {
+                        return c.UseInFrontend[Language.HTML]!.HiddenProperties.Contains(info.Name!);
+                    }
+
+                    return false;
+                });
+
+            var category = string.Empty;
+            if (info.Attributes.ContainsKey(nameof(CategoryAttribute)))
+            {
+                category = (string)info.Attributes[nameof(CategoryAttribute)].FixedArguments[0].Value!;
+            }
+
+            if (info.Attributes.ContainsKey(nameof(PropertyTabAttribute)))
+            {
+                if (!string.IsNullOrEmpty(category) && (category == previousCategory))
+                {
+                    throw new ArgumentException("Cannot set a PropertyTab in the middle of a Category");                    
+                }
+
+                var tab = (string) info.Attributes[nameof(PropertyTabAttribute)].FixedArguments[0].Value!;
+                WriteIndent(indent);
+                Output.WriteLine("<div class=\"row\">");
+                WriteIndent(indent + 1);
+                Output.WriteLine($"<label class=\"col-4 section\" i18n=\"edit|{tab}\">{tab}</label>");
+                WriteIndent(indent + 1);
+                Output.WriteLine("<div class=\"col-8\"><hr></div>");
+                WriteIndent(indent);
+                Output.WriteLine("</div>");
+            }
+
+            if (addHideOptionForMember)
+            {
+                WriteIndent(indent++);
+                Output.WriteLine($"@if (!hide{info.Name}) {{");
+            }
+
+            var type = Converter.ConvertType(info);
+            var value = "[(ngModel)]";
+            var change = "";
+
+            if (type == "file")
+            {
+                change = " (change)=\"fileChanged($event)\"";
+            }
+
+            var label = BaseTypeConverter.ToLabelCase(info.Name!);
+
+            if (info.Attributes.ContainsKey(nameof(DisplayNameAttribute)))
+            {
+                label = info.Attributes[nameof(DisplayNameAttribute)].FixedArguments[0].Value!.ToString();
+            }
+
+            var editor = "input";
+            if (info.Attributes.ContainsKey(nameof(EditorAttribute)))
+            {
+                editor = info.Attributes[nameof(EditorAttribute)].FixedArguments[0].Value!.ToString();
+            }
+
+            var fullName = $"{BaseTypeConverter.ToJSONCase(info.DefiningClass!.Name!)}.{BaseTypeConverter.ToJSONCase(info.Name!)}";
+            var noNgModel = info.IsArray || info.IsGeneric;
+
+            if (string.IsNullOrEmpty(previousCategory))
+            {
+                WriteIndent(indent);
+                Output.WriteLine("<div class=\"row\">");
+                WriteIndent(indent + 1);
+                Output.WriteLine($"<label class=\"col-4\" i18n=\"edit|{label}\">{label}</label>");
+                WriteIndent(indent + 1);
+                Output.WriteLine($"<div class=\"col-8{(string.IsNullOrEmpty(category) ? "" : $" category-{category.ToLower().Replace(" ", "-")}")}\">");
+            }
+
+            WriteIndent(indent + 2);
+            if (editor == "input")
+            {
+                var classes = "form-control";
+                if (type == "checkbox")
+                {
+                    classes += " form-check-input";
+                }
+                Output.Write($"<input class=\"{classes}\"");
+                if (!noNgModel)
+                    Output.Write($" {value}=\"{fullName}\"{change}");
+                if (!string.IsNullOrEmpty(type))
+                {
+                    Output.Write($" type=\"{type}\"");
+                }
+                Output.WriteLine($" autocomplete=\"off\" #{BaseTypeConverter.ToJSONCase(info.Name!)} id=\"{BaseTypeConverter.ToJSONCase(info.Name!)}\" />");
+            }
+            else
+            {
+                Output.Write($"<{editor} ");
+                if (!noNgModel)
+                    Output.Write($"[(ngModel)]=\"{fullName}\" ");
+                Output.WriteLine($"#{BaseTypeConverter.ToJSONCase(info.Name!)} id=\"{BaseTypeConverter.ToJSONCase(info.Name!)}\"></{editor}>");
+            }
+
+            if (string.IsNullOrEmpty(category))
+            {
+                WriteIndent(indent + 1);
+                Output.WriteLine("</div>");
+                WriteIndent(indent);
+                Output.WriteLine("</div>");
+            }
+
+            if (addHideOptionForMember)
+            {
+                WriteIndent(3);
+                Output.WriteLine("}");
+            }
+
+            previousCategory = category;
+        }
+    }
+}
--- src/Languages/HTML/HTMLFormatter.cs
+++ src/Languages/HTML/HTMLFormatter.cs
@@ -8,9 +8,11 @@ using Zestien3;
 
 namespace Z3
 {
-    internal class HTMLFormatter : BaseFormatter
+    internal abstract class HTMLFormatter : BaseFormatter
     {
-        private string previousCategory = string.Empty;
+        protected string previousCategory = string.Empty;
+        protected int buttonSectionOffset = 0;
+        protected int buttonSectionWidth = 12;
 
         /// <summary>
         /// Create an instance of the <see cref="HTMLFormatter"/> class.
@@ -158,126 +160,7 @@ namespace Z3
             // Not used for generating HTML code.
         }
 
-        private void WriteMemberInfo(MetadataMemberInfo info)
-        {
-            var indent = 3;
-
-            var addHideOptionForMember = ClassInfo!.Any(c =>
-                {
-                    if (c.UseInFrontend.ContainsKey(Language.HTML))
-                    {
-                        return c.UseInFrontend[Language.HTML]!.HiddenProperties.Contains(info.Name!);
-                    }
-
-                    return false;
-                });
-
-            var category = string.Empty;
-            if (info.Attributes.ContainsKey(nameof(CategoryAttribute)))
-            {
-                category = (string)info.Attributes[nameof(CategoryAttribute)].FixedArguments[0].Value!;
-            }
-
-            if (info.Attributes.ContainsKey(nameof(PropertyTabAttribute)))
-            {
-                if (!string.IsNullOrEmpty(category) && (category == previousCategory))
-                {
-                    throw new ArgumentException("Cannot set a PropertyTab in the middle of a Category");                    
-                }
-
-                var tab = (string) info.Attributes[nameof(PropertyTabAttribute)].FixedArguments[0].Value!;
-                WriteIndent(indent);
-                Output.WriteLine("<div class=\"row\">");
-                WriteIndent(indent + 1);
-                Output.WriteLine($"<label class=\"col-4 section\" i18n=\"edit|{tab}\">{tab}</label>");
-                WriteIndent(indent + 1);
-                Output.WriteLine("<div class=\"col-8\"><hr></div>");
-                WriteIndent(indent);
-                Output.WriteLine("</div>");
-            }
-
-            if (addHideOptionForMember)
-            {
-                WriteIndent(indent++);
-                Output.WriteLine($"@if (!hide{info.Name}) {{");
-            }
-
-            var type = Converter.ConvertType(info);
-            var value = "[(ngModel)]";
-            var change = "";
-
-            if (type == "file")
-            {
-                change = " (change)=\"fileChanged($event)\"";
-            }
-
-            var label = BaseTypeConverter.ToLabelCase(info.Name!);
-
-            if (info.Attributes.ContainsKey(nameof(DisplayNameAttribute)))
-            {
-                label = info.Attributes[nameof(DisplayNameAttribute)].FixedArguments[0].Value!.ToString();
-            }
-
-            var editor = "input";
-            if (info.Attributes.ContainsKey(nameof(EditorAttribute)))
-            {
-                editor = info.Attributes[nameof(EditorAttribute)].FixedArguments[0].Value!.ToString();
-            }
-
-            var fullName = $"{BaseTypeConverter.ToJSONCase(info.DefiningClass!.Name!)}.{BaseTypeConverter.ToJSONCase(info.Name!)}";
-            var noNgModel = info.IsArray || info.IsGeneric;
-
-            if (string.IsNullOrEmpty(previousCategory))
-            {
-                WriteIndent(indent);
-                Output.WriteLine("<div class=\"row\">");
-                WriteIndent(indent + 1);
-                Output.WriteLine($"<label class=\"col-4\" i18n=\"edit|{label}\">{label}</label>");
-                WriteIndent(indent + 1);
-                Output.WriteLine($"<div class=\"col-8{(string.IsNullOrEmpty(category) ? "" : $" category-{category.ToLower().Replace(" ", "-")}")}\">");
-            }
-
-            WriteIndent(indent + 2);
-            if (editor == "input")
-            {
-                var classes = "form-control";
-                if (type == "checkbox")
-                {
-                    classes += " form-check-input";
-                }
-                Output.Write($"<input class=\"{classes}\"");
-                if (!noNgModel)
-                    Output.Write($" {value}=\"{fullName}\"{change}");
-                if (!string.IsNullOrEmpty(type))
-                {
-                    Output.Write($" type=\"{type}\"");
-                }
-                Output.WriteLine($" autocomplete=\"off\" #{BaseTypeConverter.ToJSONCase(info.Name!)} id=\"{BaseTypeConverter.ToJSONCase(info.Name!)}\" />");
-            }
-            else
-            {
-                Output.Write($"<{editor} ");
-                if (!noNgModel)
-                    Output.Write($"[(ngModel)]=\"{fullName}\" ");
-                Output.WriteLine($"#{BaseTypeConverter.ToJSONCase(info.Name!)} id=\"{BaseTypeConverter.ToJSONCase(info.Name!)}\"></{editor}>");
-            }
-
-            if (string.IsNullOrEmpty(category))
-            {
-                WriteIndent(indent + 1);
-                Output.WriteLine("</div>");
-                WriteIndent(indent);
-                Output.WriteLine("</div>");
-            }
-
-            if (addHideOptionForMember)
-            {
-                WriteIndent(3);
-                Output.WriteLine("}");
-            }
-
-            previousCategory = category;
-        }
+        protected abstract void WriteMemberInfo(MetadataMemberInfo info);
 
         protected override void CloseClass()
         {
@@ -289,7 +172,7 @@ namespace Z3
                 WriteIndent(1);
                 Output.WriteLine("<div class=\"row\">");
                 WriteIndent(2);
-                Output.WriteLine("<div class=\"offset-4 col-8\">");
+                Output.WriteLine($"<div class=\"offset-{buttonSectionOffset} col-{buttonSectionWidth}\">");
                 WriteIndent(3);
                 Output.WriteLine("<br/>");
                 WriteIndent(3);
@@ -304,7 +187,7 @@ namespace Z3
                 Output.WriteLine("</div>");
                 Output.WriteLine("}");
             }
-       }
+        }
 
         protected override void CloseNamespace()
         {
